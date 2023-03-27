f0cc10061ee65eb032b7d13722b83bc1e5fbba06
Ghost116rus <danilkaleev@mai.ru>
2023-03-28 01:34:00 +0300

    Ещё одну задачу закрыл

---
+++ MainAplication/Core/KinoDrive.Aplication/CQRS/Bookings/Queries/GetBookingQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using KinoDrive.Aplication.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoDrive.Aplication.CQRS.Bookings.Queries
+{
+    public class GetBookingQueryHandler : IRequestHandler<GetBookingQuery, BookingPlacesListVM>
+    {
+        private readonly IKinoDriveDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetBookingQueryHandler(IKinoDriveDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<BookingPlacesListVM> Handle(GetBookingQuery request,
+            CancellationToken cancellationToken)
+        {
+            var bookingPlaces = await _context.Bookings
+                .Where(b => b.SeanceId == request.SeanceId)
+                .ProjectTo<GetBookingPlacesVM>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return new BookingPlacesListVM { Places = bookingPlaces};
+        }
+    }
+}
---
+++ MainAplication/Infrastructure/KinoDrive.Persistance/Migrations/20230327223028_AddNickName.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KinoDrive.Persistance.Migrations
+{
+    public partial class AddNickName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NickName",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "Users");
+        }
+    }
+}
