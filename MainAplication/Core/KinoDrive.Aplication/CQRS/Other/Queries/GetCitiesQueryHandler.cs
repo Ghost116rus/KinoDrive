09df97a683877faabbe306cd28c0e0948a3ee11d
Ghost116rus <danilkaleev@mai.ru>
2023-03-28 02:00:14 +0300

    Список городов высылается

---
+++ MainAplication/Core/KinoDrive.Aplication/CQRS/Other/Queries/GetCitiesQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using KinoDrive.Aplication.Common.Mappings;
+using KinoDrive.Aplication.Interfaces;
+using KinoDrive.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoDrive.Aplication.CQRS.Other.Queries
+{
+    public class GetCitiesQueryHandler : IRequestHandler<GetCitiesQuery, CityListVm>
+    {
+        private readonly IMapper mapper;
+        private readonly IKinoDriveDbContext context;
+
+        public GetCitiesQueryHandler(IMapper mapper, IKinoDriveDbContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        public async Task<CityListVm> Handle(GetCitiesQuery request,
+            CancellationToken cancellationToken)
+        {
+            var list = await context.BranchOffices
+                .Select(br => br.City)
+                .Distinct()
+                .ToListAsync();
+
+            return new CityListVm { Cities = list };
+        }
+    }
+}
---
+++ MainAplication/Presentation/KinoDriveWebAPI/Controllers/OtherConroller.cs
@@ -0,0 +1,22 @@
+﻿using KinoDrive.Aplication.CQRS.Other.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoDriveWebAPI.Controllers
+{
+    public class OtherConroller : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<CityListVm>> GetCities()
+        {
+            var query = new GetCitiesQuery();
+
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+    }
+}
