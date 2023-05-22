f5965eb024101bf8eb4a84a0bcb4051e4e9253de
Ghost116rus <danilkaleev@mai.ru>
2023-05-23 02:47:23 +0300

    Много сделано, много ещё сделать. Составление расписания выполнено, хотя и желательно будет оттестить до конца. Метод подробной информации в процессе, структуру ответа уже накидал

---
+++ MainAplication/Core/KinoDrive.Aplication/CQRS/Seanses/Commands/MakeShedule/MakeSheduleCommand.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoDrive.Aplication.CQRS.Seanses.Commands.CreateNewShedule
+{
+    public class FilmDTO
+    {
+        public int Id { get; set; }
+        public int Duration { get; set; }
+    }
+    public class SeanceDTO
+    {
+        public int Id { get; set; }
+        public int cost { get; set; }
+        public string Type { get; set; }
+        public FilmDTO Film { get; set; }
+    }
+
+    public class HallDTO
+    {
+        public int Id { get; set; }
+        public List<SeanceDTO> Seances { get; set; }
+    }
+
+
+    public class DayShedule
+    {
+        public DateTime Date { get; set; }
+        public int BranchOfficeId { get; set; }
+
+        public List<HallDTO> Halls { get; set; }
+
+    }
+
+    public class MakeSheduleCommand : IRequest
+    {
+        public DayShedule Shedule { get; set; }
+        public List<SeanceDTO>? Basket { get; set; }
+    }
+}
---
+++ MainAplication/Core/KinoDrive.Aplication/CQRS/Seanses/Queries/GetTimeTableHard/GetTimeTableHardQueryHandler.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using KinoDrive.Aplication.CQRS.Seanses.Queries.GetTimetableForWeek;
+using KinoDrive.Aplication.CQRS.Seanses.Queries.GetTimeTableHard;
+using KinoDrive.Aplication.Interfaces;
+using KinoDrive.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoDrive.Aplication.CQRS.Seanses.Queries.GetTimeTableHard
+{
+    public class GetTimeTableHardQueryHandler : IRequestHandler<GetTimeTableHardQuery, TableVM>
+    {
+        private readonly IMapper _mapper;
+        private readonly IKinoDriveDbContext _context;
+
+        public GetTimeTableHardQueryHandler(IMapper mapper, IKinoDriveDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<TableVM> Handle(GetTimeTableHardQuery request, CancellationToken cancellationToken)
+        {
+            var table = new TableVM
+            {
+                StartTime = 5,
+                EndTime = 5,
+            };
+
+            var table1 = new List<DayVm>();
+
+            var day = new DayVm()
+            {
+                Date = DateTime.Now,
+            };
+
+
+            table.Table = table1;
+            table1.Add(day);
+
+            var seance = new SeanceVM() { Id = 1 };
+
+            day.Seances.Add(seance.Id.ToString(), seance);
+
+            return table;
+        }
+    }
+}
